f680655cd1b3f7b007fc1c89a9a5d752c3c63434
Kevin Selva Prasanna <kevinselvaprasanna@gmail.com>
2021-05-27 21:22:47 +0530

    Changed the pincode api to district api

--- getapp.py
+++ getapp.py
@@ -12,45 +12,49 @@ def mynotify(text):
     Notify.Notification.new(text).show()
 
 
-url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin'
-pincode = '600091'
-day_ahead = 1
+url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict'
+pincodes = {'600091','600061','600096','600042'}
 
 
 today = datetime.date.today()
-day = today + datetime.timedelta(days=day_ahead)
+day0 = today + datetime.timedelta(days=1)
+day1 = today + datetime.timedelta(days=2)
+days = [day0, day1]
 available = 0
 
 headers = {'Accept':'application/json','Accept-Language': 'hi_IN','User-Agent':'PostmanRuntime/7.28.0'}
-params = {'pincode': pincode, 'date':day.strftime("%d-%m-%Y")}
 
 Notify.init("Covid-Vaccine Notifier")
 
-try:
-    response = requests.get(
-        url,
-        headers=headers,
-        params=params,
-    )
-    response.raise_for_status()
-except HTTPError as http_err:
-    mynotify(f'HTTP error occurred: {http_err}') 
-except Exception as err:
-    mynotify(f'Other error occurred: {err}') 
+
+for day in days:
+    params = {'district_id': 571, 'date':day.strftime("%d-%m-%Y")}
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+        )
+        response.raise_for_status()
+    except HTTPError as http_err:
+        mynotify(f'HTTP error occurred: {http_err}') 
+    except Exception as err:
+        mynotify(f'Other error occurred: {err}') 
+    else:
+        json_response = response.json()
+        sessions = json_response['sessions']
+        for session in sessions:
+            if session['available_capacity_dose1'] != 0 and session["min_age_limit"] == 18 and session["pincode"] in pincodes:
+                name = session['name'] 
+                print('Vaccine')
+                Notify.Notification.new('Vaccine Available', '✔️ ' + name).show()
+                available += 1
+            
+if available == 0:
+    print('NO')
+    Notify.Notification.new('Nothing available', '❌').show()
 else:
-    json_response = response.json()
-    sessions = json_response['sessions']
-    for i in range(len(sessions)):
-        if sessions[i]['available_capacity_dose1'] != 0 and sessions[i]["min_age_limit"] == 18:
-            name = sessions[i]['name'] 
-            print('Vaccine')
-            notification = Notify.Notification.new('Vaccine Available', '✔️ ' + name)
-            notification.show()
-            available = available + 1
-            playsound('alarm.mp3')
-        
-    if available == 0:
-        print('NO')
-        notification = Notify.Notification.new('Nothing available', '❌').show()
+    playsound('alarm.mp3')
+
 
 
